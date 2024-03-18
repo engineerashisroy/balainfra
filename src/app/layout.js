3c3e11f9c5bcf99b9e2243c68873bfddebfdea28
Ashis Roy <as.ashisroy@gmail.com>
2024-03-18 12:30:04 +0530

    all layout create

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
+import SiteNavbar from "@/components/SiteNavbar";
 import "./globals.css";
+import SiteFooter from "@/components/SiteFooter";
 
 export const metadata = {
   title: "Create Next App",
@@ -8,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <SiteNavbar></SiteNavbar>
+        {children}
+        <SiteFooter></SiteFooter>
+      </body>
     </html>
   );
 }
---
+++ src/components/SiteNavbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SiteNavbar = () => {
+  return <div>SiteNavbar</div>;
+};
+
+export default SiteNavbar;
