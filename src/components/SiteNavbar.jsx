e4b901e8670b171ce719d506076643ba966bfcc4
Ashis Roy <as.ashisroy@gmail.com>
2024-03-18 17:07:03 +0530

    model update

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -1,6 +1,27 @@
 "use client";
 
+import ModelPopup from "./ModelPopup";
+
 const SiteNavbar = () => {
+  const menuItem = (
+    <>
+      <li>
+        <a>Home</a>
+      </li>
+      <li>
+        <a>About Us</a>
+      </li>
+      <li>
+        <a>Projects</a>
+      </li>
+      <li>
+        <a>Services</a>
+      </li>
+      <li>
+        <a>Careers</a>
+      </li>
+    </>
+  );
   return (
     <div className="navbar bg-base-100">
       <div className="navbar-start">
@@ -25,53 +46,23 @@ const SiteNavbar = () => {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
+            {menuItem}
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl text-primary">Bala Infra</a>
       </div>
       <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li>
-            <details>
-              <summary>Parent</summary>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </details>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
+        <ul className="menu menu-horizontal px-1">{menuItem}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn">Button</a>
+        <a
+          className="btn "
+          onClick={() => document.getElementById("getInTouch").showModal()}
+        >
+          Get in touch
+        </a>
       </div>
+      {<ModelPopup></ModelPopup>}
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      themeColor: {
+        primary: "33FFD4",
+        secondary: "33D7FF",
+      },
     },
   },
   plugins: [require("daisyui")],
