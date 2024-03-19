231e4ba8a31bc48278e74e2e485f7c907e8220db
developerashisroy <ashisroy40089@gmail.com>
2024-03-19 14:40:54 +0530

    about us page work

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const About = () => {
+export const page = () => {
   return (
-    <div>About</div>
+      <div className='container mx-auto'>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+              <div className="one1">one</div>
+              <div className="one2">two</div>
+              <div className="one3">three</div>
+          </div>
+      </div>
   )
 }
 
-export default About
+export default page
